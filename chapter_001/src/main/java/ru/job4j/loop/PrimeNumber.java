27ee8ed9ab61a3be8fb8fcf0e302921a8dbeb811
dobryansky <mikstrip@yandex.ru>
2020-01-29 22:05:19 +0300

    5.6. Простые числа.[#226152]

---
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+    // т.к. мы проверяем от 1 до x, а число 1 не является простым, но проходит в методе CheckPrimeNumber, как true, то мы начальное значение счетчика count устанавливаем -1
+
+    public int calc(int finish) {
+        int count = -1;
+        for (int number =1;number<=finish;number++ ) {
+            if (CheckPrimeNumber.check(number)==true) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
