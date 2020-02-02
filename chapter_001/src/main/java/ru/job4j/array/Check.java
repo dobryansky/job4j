b16b3737a2b849e44ad962fc8b1000f1f16b65ea
dobryansky <mikstrip@yandex.ru>
2020-02-02 19:39:53 +0300

    6.3. Массив заполнен true или false[#226095]

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -1,4 +1,4 @@
-package ru.job4j.array;
+/* package ru.job4j.array;
 
 public class Check {
     public static boolean mono(boolean[] data) {
@@ -19,6 +19,30 @@ public class Check {
         {
             result=true;
         }
+
+        return result;
+    }
+}
+
+ */
+
+package ru.job4j.array;
+
+public class Check {
+    public static boolean mono(boolean[] data) {
+        boolean result = false;
+
+        for (int i=0;i<data.length;i++) {
+            if (data[0]==data[i])
+            {
+               result=true;
+            }
+         else {
+             result=false;
+             break;
+            }
+        }
+
 
         return result;
     }
--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -1,6 +1,10 @@
 package ru.job4j.array;
 
 public class FindLoop {
+    public static int indexOf(int[] data, int el) {
+        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
+        return rst;
+    }
 
 
     public static int indexOf(int[] data, int el, int start, int finish) {
