d64c6400a72fb66fdfaf536c4411fc37ebd9a774
ARTEM <mikstrip@yandex.ru>
2020-02-04 16:23:17 +0300

    6.8. Дефрагментация массива.[#226106]

--- chapter_001/src/main/java/ru/job4j/array/Defragment.java
+++ chapter_001/src/main/java/ru/job4j/array/Defragment.java
@@ -6,14 +6,27 @@ public class Defragment {
             if (array[index] == null) {
 
                 String temp;
-                for (int j = index + 1; j < array.length; j++) {
+               /* for (int j = index + 1; j < array.length; j++) {
                     if (array[j] != null) {
                         temp = array[j];
                         array[j] = array[index];
                         array[index] = temp;
                         break;
                     }
+                } */
+                int j = index + 1;
+                while (j < array.length) {
+                    if (array[j] != null) {
+                        temp = array[j];
+                        array[j] = array[index];
+                        array[index] = temp;
+
+                        break;
+                    }
+                    j++;
                 }
+
+
             }
             System.out.print(array[index] + " ");
         }
@@ -21,7 +34,7 @@ public class Defragment {
     }
 
     public static void main(String[] args) {
-        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed", null, null, "by Dobryansky", null};
         String[] compressed = compress(input);
         System.out.println();
         for (int index = 0; index < compressed.length; index++) {
