6377df2b3a956fa38ce5e9714d06546effa32968
ARTEM <mikstrip@yandex.ru>
2020-02-12 15:47:17 +0300

    1.5. Поля объекта[#226249]

--- chapter_001/src/main/java/ru/job4j/oop/Cat.java
+++ chapter_001/src/main/java/ru/job4j/oop/Cat.java
@@ -4,15 +4,18 @@ public class Cat {
 
     private String food;
     private String name;
+
     public void show() {
         System.out.println(this.name);
         System.out.println(this.food);
     }
+
     public void eat(String meat) {
         this.food = meat;
     }
-    public void giveNick(String nick){
-        this.name=nick;
+
+    public void giveNick(String nick) {
+        this.name = nick;
     }
 
     public static void main(String[] args) {
