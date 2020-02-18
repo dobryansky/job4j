8c6f1d4828f249a6694ed3fd4182e88a4ba48159
dobryansky <mikstrip@yandex.ru>
2020-02-18 22:06:14 +0300

    1. Конструктор по умолчанию.[#226259]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/Predator.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker;
+
+public class Predator extends Animal {
+    public Predator() {
+
+        System.out.println("Predator");
+
+    }
+
+
+
+}
---
+++ chapter_002/src/main/java/ru/job4j/tracker/Tiger.java
@@ -0,0 +1,11 @@
+package ru.job4j.tracker;
+
+public class Tiger extends Predator {
+    public Tiger(){
+
+        System.out.println("Tiger");
+
+
+    }
+
+}
