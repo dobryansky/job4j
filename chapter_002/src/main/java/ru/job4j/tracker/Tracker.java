2fdb9e8d80515bfaadf96c15062b15ff0e161110
ARTEM <mikstrip@yandex.ru>
2020-02-26 15:46:55 +0300

    3. Массивы и модели.[#226267]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -0,0 +1,55 @@
+package ru.job4j.tracker;
+
+import java.util.Random;
+
+public class Tracker {
+    /**
+     * Массив для хранения заявок.
+     */
+    private final Item[] items = new Item[100];
+
+    /**
+     * Указатель ячейки для новой заявки.
+     */
+    private int position = 0;
+
+    /**
+     * Метод добавления заявки в хранилище
+     * @param item новая заявка
+     */
+    public Item add(Item item) {
+        item.setId(this.generateId());
+        items[this.position++] = item;
+        return item;
+    }
+
+    /**
+     * Метод генерирует уникальный ключ для заявки.
+     * Так как у заявки нет уникальности полей, имени и описание. Для идентификации нам нужен уникальный ключ.
+     * @return Уникальный ключ.
+     */
+    private String generateId() {
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
+
+    }
+
+    public Item[] findAll(){
+        for(int i=0;i<position;i++){
+            int name = items[i];
+            if (name != null) {
+                namesWithoutNull[size] = name;
+                size++;
+            }
+        }
+
+    }
+
+    public Item[] findByName(String key){
+
+    }
+
+    public Item findById(String id){
+
+    }
+}
