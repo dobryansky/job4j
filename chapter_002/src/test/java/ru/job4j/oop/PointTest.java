fee0e4deb801a929423c6a50decd93ebdb811aa1
ARTEM <mikstrip@yandex.ru>
2020-03-02 17:43:23 +0300

    трекер крекер

--- chapter_002/src/test/java/ru/job4j/oop/PointTest.java
+++ chapter_002/src/test/java/ru/job4j/oop/PointTest.java
@@ -1,22 +1,20 @@
 package ru.job4j.oop;
 import org.junit.Assert;
 import org.junit.Test;
-import ru.job4j.oop.Point;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
 
 public class PointTest {
     public PointTest(){
 
     }
     @Test
+    
     public void distance () {
         int x1=0;
         int x2=0;
         int y1=2;
         int y2=0;
         Point point1 = new Point(x1,y1);
-        Point point2 = new Point(x1,y2);
+        Point point2 = new Point(x2,y2);
         double out = point1.distance(point2);
         double expected = 2;
         Assert.assertEquals(expected,out,0.01D);
