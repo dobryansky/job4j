67682828d906db7c61a76899976511e7dc50093d
dobryansky <mikstrip@yandex.ru>
2020-03-18 22:20:39 +0300

    0.2. Кидаем исключение - throw new RuntimeException[#226235]

---
+++ chapter_002/src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,25 @@
+package ru.job4j.ex;
+
+import java.util.Scanner;
+
+public class Fact {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите число - ");
+        int n=scan.nextInt();
+        Fact fact=new Fact();
+        System.out.println("Factorial of " + n + " равен - "+fact.calc(n));
+
+    }
+
+    public int calc(int n) {
+        if (n<1){
+            throw new IllegalArgumentException("Argument must be >= 1");
+        }
+        int rsl = 1;
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+}
