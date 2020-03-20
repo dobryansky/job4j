46e89a5949787b428c20d1f89814715fa48e82da
dobryansky <mikstrip@yandex.ru>
2020-03-20 14:34:18 +0300

    Пользовательские исключения.[#226237]. Исправление №1

--- chapter_002/src/main/java/ru/job4j/ex/FindEl.java
+++ chapter_002/src/main/java/ru/job4j/ex/FindEl.java
@@ -1,31 +1,29 @@
 package ru.job4j.ex;
 
-public class FindEl  {
-    public  int indexOf(String[] value, String key) throws ElementNotFoundException     {
+public class FindEl {
+    public int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
-        int counter=0;
-        for (String i: value){
-            if (i.equals(key)){
-               rsl++;
-               break;
+        int counter = 0;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
             }
-            rsl++;
-            counter++;
-            if(counter==value.length){
-                rsl = -1;
-                throw new ElementNotFoundException("There is no elements with such id");
-            }
+
         }
-        System.out.println("Индекс равен - "+rsl);
+        if (rsl == -1) {
+            throw new ElementNotFoundException("There is no elements with such id");
+        }
+        System.out.println("Индекс равен - " + rsl);
+
         return rsl;
     }
+
     public static void main(String[] args) {
-        String[] value= {"weqew","wrerwer","dads","dfgdf","fgh","ghjg"};
-        FindEl findEl=new FindEl();
+        String[] value = {"weqew", "wrerwer", "dads", "dfgdf", "fgh", "ghjg"};
+        FindEl findEl = new FindEl();
         try {
-            findEl.indexOf(value, "ghjg");
-        }
-        catch (ElementNotFoundException e){
+            findEl.indexOf(value, "dads");
+        } catch (ElementNotFoundException e) {
             e.printStackTrace();
         }
 
--- chapter_002/src/test/java/ru/job4j/ex/IndexofTest.java
+++ chapter_002/src/test/java/ru/job4j/ex/IndexofTest.java
@@ -1,36 +1,38 @@
 
 package ru.job4j.ex;
+
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-public class IndexofTest  {
-   @Test
+public class IndexofTest {
+    @Test
     public void Index0() throws ElementNotFoundException {
-        String[] array={"qewqw","qweqw","ewr","tryhr","sbd","dmtnm","vaewrv","tdymn","gaerg","fcaw","cwcf"};
-        FindEl findEl=new FindEl();
+        String[] array = {"qewqw", "qweqw", "ewr", "tryhr", "sbd", "dmtnm", "vaewrv", "tdymn", "gaerg", "fcaw", "cwcf"};
+        FindEl findEl = new FindEl();
 
-        int expected=findEl.indexOf(array,"tryhr");
+        int expected = findEl.indexOf(array, "tryhr");
         assertThat(expected, is(3));
 
     }
 
     @Test
     public void IndexLast() throws ElementNotFoundException {
-        String[] array={"qewqw","efvca","ewr","tryhr","sbd","dmtnm","vaewrv","tdymn","gaerg","fcaw","cwcf"};
-        FindEl findEl=new FindEl();
+        String[] array = {"qewqw", "efvca", "ewr", "tryhr", "sbd", "dmtnm", "vaewrv", "tdymn", "gaerg", "fcaw", "cwcf"};
+        FindEl findEl = new FindEl();
 
-        int expected=findEl.indexOf(array,"cwcf");
+        int expected = findEl.indexOf(array, "cwcf");
         assertThat(expected, is(10));
 
     }
+
     @Test(expected = ElementNotFoundException.class)
     public void IndexNO() throws ElementNotFoundException {
-        String[] array={"qewqw","qweqw","ewr","tryhr","sbd","dmtnm","vaewrv","tdymn","gaerg","fcaw","cwcf"};
-        FindEl findEl=new FindEl();
+        String[] array = {"qewqw", "qweqw", "ewr", "tryhr", "sbd", "dmtnm", "vaewrv", "tdymn", "gaerg", "fcaw", "cwcf"};
+        FindEl findEl = new FindEl();
 
-        findEl.indexOf(array,"try69hr");
+        findEl.indexOf(array, "try69hr");
 
 
     }
