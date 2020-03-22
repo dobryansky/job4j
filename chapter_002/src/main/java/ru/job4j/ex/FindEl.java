27d570195ac0a6469a85d94cb47e851b08c27c84
dobryansky <mikstrip@yandex.ru>
2020-03-22 19:38:13 +0300

    Пользовательские исключения. вер.3

--- chapter_002/src/main/java/ru/job4j/ex/FindEl.java
+++ chapter_002/src/main/java/ru/job4j/ex/FindEl.java
@@ -3,18 +3,16 @@ package ru.job4j.ex;
 public class FindEl {
     public int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
-        int counter = 0;
         for (int i = 0; i < value.length; i++) {
             if (value[i].equals(key)) {
                 rsl = i;
+                break;
             }
-
         }
         if (rsl == -1) {
             throw new ElementNotFoundException("There is no elements with such id");
         }
         System.out.println("Индекс равен - " + rsl);
-
         return rsl;
     }
 
@@ -22,7 +20,7 @@ public class FindEl {
         String[] value = {"weqew", "wrerwer", "dads", "dfgdf", "fgh", "ghjg"};
         FindEl findEl = new FindEl();
         try {
-            findEl.indexOf(value, "dads");
+            findEl.indexOf(value, "ghjg");
         } catch (ElementNotFoundException e) {
             e.printStackTrace();
         }
