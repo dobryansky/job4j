18d73900bcb3a99cf993d4ae025b1c064e21fb1a
dobryansky <mikstrip@yandex.ru>
2020-04-03 20:34:51 +0300

    Конвертация ArrayList в двухмерный массив/Конвертация двумерного массива в ArrayList

---
+++ chapter_003/src/main/java/collection/ConvertList2Array.java
@@ -0,0 +1,32 @@
+package collection;
+
+import java.util.List;
+
+public class ConvertList2Array {
+    public static int[][] toArray(List<Integer> list, int cells) {
+        int groups = (int) Math.ceil((double) list.size() / cells);
+        int[][] array = new int[groups][cells];
+        int row = 0, cell = 0;
+        for (Integer num : list) {
+            array[row][cell] = num;
+            cell++;
+            if (cell == cells) {
+                row++;
+                cell = 0;
+            }
+
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = List.of(10, 9, 8, 7, 6, 5, 4,9,10,11);
+        int[][] rsl = toArray(list, 3);
+        for (int[] row : rsl) {
+            for (int cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ chapter_003/src/main/java/list/ConvertMatrix2List.java
@@ -0,0 +1,18 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConvertMatrix2List {
+    public List<Integer> toList(int[][] array) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int[] innerArray : array) {
+            for (int data : innerArray) {
+                list.add(data);
+            }
+
+        }
+        return list;
+    }
+}
