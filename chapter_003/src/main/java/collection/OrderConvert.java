e96061dac0e6ee0b8b5da6a668e338922fe1366e
dobryansky <mikstrip@yandex.ru>
2020-04-10 20:46:02 +0300

    2. Преобразования List в Map.[#226193]

---
+++ chapter_003/src/main/java/collection/OrderConvert.java
@@ -0,0 +1,20 @@
+package collection;
+
+
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class OrderConvert {
+    public static HashMap<String, Order> process(List<Order> orders) {
+        HashMap<String, Order> map = new HashMap<>();
+
+
+
+        for (Order order : orders) {
+            map.put(order.getNumber(), order);
+        }
+        return map;
+    }
+}
